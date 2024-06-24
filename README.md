9b7488c95ca471201f836e4066e39c1641ddfdac
Brazatanu Borah <136308834+Brazatanu-Borah@users.noreply.github.com>
2024-06-24 14:56:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# super-basic-age-calculator
+ A simple JavaScript program that calculates the number of days someone has lived based on their current age.
---
+++ main.js
@@ -0,0 +1,5 @@
+	  var age = prompt("What is your age? ");
+
+	  var days = age * 365.25; // factors in leap years
+
+	  alert(age + " years is roughly " + days + " days");
